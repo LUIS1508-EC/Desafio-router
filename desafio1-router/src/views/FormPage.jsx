35a56ee0332696d36c05017b734989be8e174d49
LUIS1508-EC <151102084+LUIS1508-EC@users.noreply.github.com>
2024-06-02 07:52:57 -0400

    .

--- desafio1-router/src/views/FormPage.jsx
+++ desafio1-router/src/views/FormPage.jsx
@@ -3,7 +3,7 @@ import Contacto from '../components/Formulario';
 
 function FormPage() {
   return (
-    <Container className="pt-5">
+    <Container className="pt-5 text-center d-block">
       <h1 className="mb-4">Escribenos</h1>
       <Contacto />
     </Container>
--- desafio1-router/src/views/HomePage.jsx
+++ desafio1-router/src/views/HomePage.jsx
@@ -1,12 +1,14 @@
 import { Container } from "react-bootstrap";
+import happyCake from '../../../assets/img/happyCake.png'
 
 const HomePage = () => {
   return (
-    <Container className="text-center">
+    <Container className="container-fluid text-center">
       <h1 className="pt-5">
-        Bienvenido a <span className="fw-bold">Hapy Cake</span> 🐶
+        Bienvenido a <span className="fw-bold">Hapy Cake</span>
       </h1>
       <h6> El lugar de los pasteles felices </h6>
+      <img src={happyCake} height="200px" />
     </Container>
   );
 };
